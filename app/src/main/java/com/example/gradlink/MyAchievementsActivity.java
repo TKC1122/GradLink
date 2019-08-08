0952e418a71e1bedb35e1ef11d90a7a070a33e4e
TKC1122 <52437816+TKC1122@users.noreply.github.com>
2019-08-09 01:05:45 +1000

    Update MyAchievementsActivity.java

--- app/src/main/java/com/example/gradlink/MyAchievementsActivity.java
+++ app/src/main/java/com/example/gradlink/MyAchievementsActivity.java
@@ -1,12 +1,17 @@
 package com.example.gradlink;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 
 public class MyAchievementsActivity extends NavigationActivity {
+
+    private static Button questionbutton1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +26,22 @@ public class MyAchievementsActivity extends NavigationActivity {
         View contentView = inflater.inflate(R.layout.activity_my_achievements, null, false);
         drawer.addView(contentView, 0);
         navigationView.setCheckedItem(R.id.myachievementsnav);
+        OnClickButtonListener();
+
     }
+
+
+    public void OnClickButtonListener() {
+        questionbutton1 = (Button)findViewById(R.id.questionbutton1);
+        questionbutton1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.gradlink.MyAchievementsSecond");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
 }
